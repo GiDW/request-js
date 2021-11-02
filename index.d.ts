ba063fb6dac4aaa00174fb90553e8e5632792875
Gilles De Waele <gilles.dewaele@basalte.be>
2021-11-02 16:21:58 +0100

    Code style .d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,42 +1,42 @@
 export interface RequestJsConfig {
-    url: string;
-    method?: string;
-    params?: {
-        [propName: string]: any;
-    };
-    headers?: {
-        [propName: string]: string | number | boolean | null;
-    };
-    timeout?: number;
-    data?: any;
-    json?: boolean;
+  url: string
+  method?: string
+  params?: {
+    [propName: string]: any
+  }
+  headers?: {
+    [propName: string]: string | number | boolean | null
+  }
+  timeout?: number
+  data?: any
+  json?: boolean
 }
 export interface RequestJsCallback {
-    (error: RequestJsResult | null, result?: RequestJsResult): void;
+  (error: RequestJsResult | null, result?: RequestJsResult): void
 }
 export interface RequestJsResult {
-    data: string;
-    config: RequestJsConfig;
-    status: number;
-    statusText: string;
-    headers: string;
-    requestStatus: string;
+  data: string
+  config: RequestJsConfig
+  status: number
+  statusText: string
+  headers: string
+  requestStatus: string
 }
 export interface RequestJsRequest {
-    abort: () => void;
+  abort: () => void
 }
 declare function RequestJs(
   config: string | RequestJsConfig,
   callback: RequestJsCallback
-): RequestJsRequest;
+): RequestJsRequest
 declare namespace RequestJs {
-    var parseHeaders: (headers: string | {
-        [key: string]: any;
-    }) => {
-        [key: string]: string;
-    };
-    var ERROR: string;
-    var TIMEOUT: string;
-    var COMPLETED: string;
+  var parseHeaders: (headers: string | {
+    [key: string]: any
+  }) => {
+    [key: string]: string
+  }
+  var ERROR: string
+  var TIMEOUT: string
+  var COMPLETED: string
 }
-export default RequestJs;
+export default RequestJs
